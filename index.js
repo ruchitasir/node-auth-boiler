6eba33052c213d970954755cc9b2afe6b3bb1af9
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-04-13 10:33:29 -0700

    added home page and nav bar content

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+/*******************************************
+ * REQUIRED
+ ******************************************/
 // Require needed modules
 let express = require('express')
 let layouts = require('express-ejs-layouts')
 
 // creata an app instance
 let app = express()
+/*******************************************
+ * SETTINGS/MIDDLEWARE
+ ******************************************/
 
 // set template language to ejs
 app.set('view engine','ejs')
-
 //Tell express to use the layout modules
 app.use(layouts)
-
 // Set up static folder with 
 app.use(express.static('static'))
 
+/*******************************************
+ * ROUTES
+ ******************************************/
+// create a home page
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+ // create a wildcard route
 app.get('*',(req,res)=>{
     //res.send("This matches literally anything, great spot for an error page")
     res.render('error');
 })
 
+/*******************************************
+ * LISTEN
+ ******************************************/
 app.listen(3000)
