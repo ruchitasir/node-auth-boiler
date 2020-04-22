fe435592c99edd7a4bf479e91db9a67cb5288d08
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-04-22 12:36:24 -0700

    Sign up form added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,33 @@
 let router = require('express').Router();
 
 //Routes
+//GET /auth/login
 router.get('/login',(req,res)=>{
      res.render('auth/login')
 })
 
-// Post auth login
+// Post /auth/login
 router.post('/login',(req,res)=>{
     console.log('data', req.body)
     res.send("Hello post")
 })
-
+// GET /auth/signup- render the signup page
 router.get('/signup',(req,res)=>{
-    res.render('auth/signup')
+    res.render('auth/signup',{data: {}})
 })
 
+//POST /auth/signup
+router.post('/signup',(req,res)=>{
+    console.log('data', req.body)
+    if(req.body.password !== req.body.password_verify)
+    {
+        // Send the message that passwords dont match
+        //Put the user back onto the signup form to try again
+        res.render('auth/signup',{data: req.body})
+    }
+    else{
+    res.send("Signup successful")
+    }
+})
 //Export (allow me to include this in another page)
 module.exports = router;
